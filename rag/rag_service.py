33c49f911e9c99b2498bdd9051a20ea3313372ab
barnabaclements <barnabaclements@gmail.com>
2026-07-13 15:25:22 +0530

    Lazy load embedding model

--- rag/rag_service.py
+++ rag/rag_service.py
@@ -1,6 +1,4 @@
 import os
-import shutil
-
 
 from django.conf import settings
 
@@ -8,7 +6,6 @@ from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 
 from langchain_groq import ChatGroq
-
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_chroma import Chroma
 from langchain_core.prompts import ChatPromptTemplate
@@ -22,15 +19,21 @@ UPLOAD_DIR = settings.UPLOAD_DIR
 DB_DIR = settings.CHROMA_DB_DIR
 
 
-embedding_model = HuggingFaceEmbeddings(
-    model_name="sentence-transformers/all-MiniLM-L6-v2"
-)
-
 llm = ChatGroq(
     groq_api_key=settings.GROQ_API_KEY,
     model_name="llama-3.3-70b-versatile",
-    temperature=0
+    temperature=0,
 )
+
+
+def get_embedding_model():
+    """
+    Load the embedding model only when needed.
+    This avoids loading torch during Django startup.
+    """
+    return HuggingFaceEmbeddings(
+        model_name="sentence-transformers/all-MiniLM-L6-v2"
+    )
 
 
 # -----------------------------
@@ -38,19 +41,13 @@ llm = ChatGroq(
 # -----------------------------
 def process_pdf(pdf_path):
     """
-    Load PDF, split it into chunks,
-    create embeddings and store them in Chroma.
+    Load PDF, split into chunks,
+    create embeddings and save them in Chroma.
     """
 
-    # Delete previous vector database
-  #  if os.path.exists(DB_DIR):
-   #     shutil.rmtree(DB_DIR)
-
-    # Load PDF
     loader = PyPDFLoader(pdf_path)
     documents = loader.load()
 
-    # Split document
     splitter = RecursiveCharacterTextSplitter(
         chunk_size=1000,
         chunk_overlap=200,
@@ -58,7 +55,8 @@ def process_pdf(pdf_path):
 
     chunks = splitter.split_documents(documents)
 
-    # Store in Chroma
+    embedding_model = get_embedding_model()
+
     Chroma.from_documents(
         documents=chunks,
         embedding=embedding_model,
@@ -73,12 +71,13 @@ def process_pdf(pdf_path):
 # -----------------------------
 def ask_question(question):
     """
-    Search relevant chunks
-    and ask Llama3.
+    Search relevant chunks and ask Groq.
     """
 
     if not os.path.exists(DB_DIR):
         return "Please upload a PDF first."
+
+    embedding_model = get_embedding_model()
 
     vectordb = Chroma(
         persist_directory=DB_DIR,
@@ -93,7 +92,6 @@ def ask_question(question):
 
     context = "\n\n".join(
         doc.page_content
-        
         for doc in docs
     )
 
@@ -103,8 +101,7 @@ You are an AI assistant.
 
 Answer ONLY from the provided context.
 
-If the answer is not found in the context,
-say:
+If the answer is not found in the context, say:
 
 "I couldn't find the answer in the uploaded document."
 
